fbd2df898b1ab887bd3228ddf5f1993f3f685ce2
2Tucho <albergromero@gmail.com>
2025-06-05 16:43:57 +0200

    Added uploads up to exercise 3.12

--- index.js
+++ index.js
@@ -1,75 +1,68 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
-const cors = require('cors')
 const Note = require('./models/note')
 
-app.use(cors())
+const app = express()
+
+let notes = []
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 app.use(express.static('dist'))
 app.use(express.json())
-
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
+  Note.find({}).then((notes) => {
     response.json(notes)
   })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) // La variable id contiene una cadena '1', mientras que los ids de las notas son números enteros
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) // La variable id contiene una cadena '1', mientras que los ids de las notas son números enteros
-  notes = notes.filter(note => note.id !== id)
+  const id = request.params.id
+  notes = notes.filter((note) => note.id !== id)
 
   response.status(204).end()
 })
 
-//Agreguemos el siguiente middleware después de nuestras rutas, que se usa para capturar solicitudes realizadas a rutas inexistentes. Para estas solicitudes, el middleware devolverá un mensaje de error en formato JSON.
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint) //Agreguemos el siguiente middleware después de nuestras rutas, que se usa para capturar solicitudes realizadas a rutas inexistentes. Para estas solicitudes, el middleware devolverá un mensaje de error en formato JSON.
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
