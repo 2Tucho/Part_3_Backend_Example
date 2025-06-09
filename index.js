72fe320c9d275b1aa122c1fb7d9d5f74b7a731b0
2Tucho <albergromero@gmail.com>
2025-06-09 12:14:43 +0200

    Updated until exercise 3.19

--- index.js
+++ index.js
@@ -32,33 +32,27 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 //POST NEW NOTE http://localhost:3001/api/notes
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ error: "content missing" })
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
+  note.save()
+      .then((savedNote) => {
     response.json(savedNote)
   })
+      .catch(error => next(error))
 })
 
 // Cambiar de importancia las notas
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true }) // Agregamos el parámetro opcional { new: true }, que hará que nuestro controlador de eventos sea llamado con el nuevo documento modificado en lugar del original.
+  Note.findByIdAndUpdate(request.params.id, { content, important },
+    { new: true, runValidators: true, context: 'query' }) // Agregamos el parámetro opcional { new: true }, que hará que nuestro controlador de eventos sea llamado con el nuevo documento modificado en lugar del original.
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -84,6 +78,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   } 
 
   next(error)
